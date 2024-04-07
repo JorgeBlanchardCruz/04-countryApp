e8f540566b949cda21b88a44f03e79f2f4577090
Jorge Blanchard Cruz <rothgard@live.com>
2024-04-07 11:57:36 +0100

    next

---
+++ src/app/countries/countries-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
+
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+
+const rountes : Route[] = [
+  {
+    path: 'by-capital',
+    component: ByCapitalPageComponent
+  },
+  {
+    path: 'by-country',
+    component: ByCountryPageComponent
+  },
+  {
+    path: 'by-region',
+    component: ByRegionPageComponent
+  },
+  {
+    path: 'by/:id',
+    component: CountryPageComponent
+  },
+]
+
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(rountes)
+  ],
+  exports: [
+    RouterModule
+  ],
+})
+export class CountriesRoutingModule { }
